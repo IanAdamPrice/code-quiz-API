f9bc595dc8039de98bd34e407871a0d1407e0111
IanAdamPrice <ianadamprice@gmail.com>
2022-03-01 16:15:19 -0500

    finished adding questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var startEl = document.querySelector("#quizBody");
 var timerEl = document.querySelector("#timer");
 var time = 75;
+var score = 0;
+
+var score = function () {
+    totalScore = totalScore + 10;
+}
 
 
 var timer = function () {
@@ -16,41 +21,284 @@ var timer = function () {
     }, 1000);
 };
 
-var questionOne = function() {
+var questionFive = function () {
+    var wrong = function () {
+        questionDiv.remove();
+    }
 
-    var questionOneEl = document.createElement("div");
-    questionOneEl.className = "question-one-container";
-    startEl.appendChild(questionOneEl);
+    var questionDiv = document.createElement("div");
+    questionDiv.className = "questionDiv";
+    startEl.appendChild(questionDiv);
 
-    var questionOne = document.createElement("h1");
-    questionOne.textContent = "Inside which HTML element do we put the JavaScript?";
-    questionOne.className = "question-one";
-    questionOneEl.appendChild(questionOne);
+    var question =document.createElement("h1");
+    question.className = "question";
+    question.textContent = "What are the main differences between let and const?";
+    questionDiv.appendChild(question);
 
     var answerDiv = document.createElement("div");
-    answerDiv.className = "answer-container";
-    questionOneEl.appendChild(answerDiv);
+    answerDiv.className = "answerDiv";
+    questionDiv.appendChild(answerDiv);
 
-    var answerOne = document.createElement("button");
-    answerOne.textContent = "1. script";
-    answerOne.className = "answer-one";
-    answerDiv.appendChild(answerOne);
+    var answer1 = document.createElement("button");
+    answer1.className = "answer-one";
+    answer1.textContent = "1. let can be reassigned, const cannot be reassigned";
+    answerDiv.appendChild(answer1);
+    answer1.addEventListener("click", event => {
+        questionDiv.remove();
+        points();
+    })
 
-    var answerTwo = document.createElement("button");
-    answerTwo.textContent = "2. js";
-    answerTwo.className = "answer-two"
-    answerDiv.appendChild(answerTwo);
+    var answer2 = document.createElement("button");
+    answer2.className = "answer-two";
+    answer2.textContent = "2. let cannot be reassigned, const can be reassigned";
+    answerDiv.appendChild(answer2);
+    answer2.addEventListener("click", event => {
+        wrong();
+    });
 
-    var answerThree = document.createElement("button");
-    answerThree.textContent = "3. scripting";
-    answerThree.className = "answer-three"
-    answerDiv.appendChild(answerThree);
+    var answer3 = document.createElement("button");
+    answer3.className = "answer-three";
+    answer3.textContent = "3. let is functional scope, while const is block scope";
+    answerDiv.appendChild(answer3);
+    answer3.addEventListener("click", event => {
+        wrong();
+    });
 
-    var answerFour = document.createElement("button");
-    answerFour.textContent = "4. javascript";
-    answerFour.className = "answer-four";
-    answerDiv.appendChild(answerFour);
-}
+    var answer4 = document.createElement("button");
+    answer4.className = "answer-four";
+    answer4.textContent = "4. let is block scope, while const is functional scope";
+    answerDiv.appendChild(answer4);
+    answer4.addEventListener("click", event => {
+        wrong();
+    });
+};
+
+var questionFour = function () {
+    var wrong = function () {
+        questionFive();
+        questionDiv.remove();
+        time = time - 10;
+    }
+
+    var questionDiv = document.createElement("div");
+    questionDiv.className = "questionDiv";
+    startEl.appendChild(questionDiv);
+
+    var question =document.createElement("h1");
+    question.className = "question";
+    question.textContent = "How do you round the number 7.25, to the nearest integer?";
+    questionDiv.appendChild(question);
+
+    var answerDiv = document.createElement("div");
+    answerDiv.className = "answerDiv";
+    questionDiv.appendChild(answerDiv);
+
+    var answer1 = document.createElement("button");
+    answer1.className = "answer-one";
+    answer1.textContent = "1. Math.rnd(7.25)";
+    answerDiv.appendChild(answer1);
+    answer1.addEventListener("click", event => {
+        wrong();
+    });
+
+    var answer2 = document.createElement("button");
+    answer2.className = "answer-two";
+    answer2.textContent = "2. round(7.25)";
+    answerDiv.appendChild(answer2);
+    answer2.addEventListener("click", event => {
+        wrong();
+    });
+
+    var answer3 = document.createElement("button");
+    answer3.className = "answer-three";
+    answer3.textContent = "3. rnd(7.25)";
+    answerDiv.appendChild(answer3);
+    answer3.addEventListener("click", event => {
+        wrong();
+    });
+
+    var answer4 = document.createElement("button");
+    answer4.className = "answer-four";
+    answer4.textContent = "4. Math.round(7.25)";
+    answerDiv.appendChild(answer4);
+    answer4.addEventListener("click", event => {
+        questionDiv.remove();
+        questionFive();
+        points();
+    })
+};
+
+var questionThree = function () {
+    var wrong = function () {
+        questionFour();
+        questionDiv.remove();
+        time = time - 10;
+    }
+
+    var questionDiv = document.createElement("div");
+    questionDiv.className = "questionDiv";
+    startEl.appendChild(questionDiv);
+
+    var question =document.createElement("h1");
+    question.className = "question";
+    question.textContent = "What is the correct way to write a JavaScript array";
+    questionDiv.appendChild(question);
+
+    var answerDiv = document.createElement("div");
+    answerDiv.className = "answerDiv";
+    questionDiv.appendChild(answerDiv);
+
+    var answer1 = document.createElement("button");
+    answer1.className = "answer-one";
+    answer1.textContent = "1. var colors = ['red', 'green', 'blue']";
+    answerDiv.appendChild(answer1);
+    answer1.addEventListener("click", event => {
+        questionDiv.remove();
+        questionFour();
+        points();
+    })
+
+    var answer2 = document.createElement("button");
+    answer2.className = "answer-two";
+    answer2.textContent = "2. var colors = 1 = ('red'),2 = ('green')";
+    answerDiv.appendChild(answer2);
+    answer2.addEventListener("click", event => {
+        wrong();
+    });
+
+    var answer3 = document.createElement("button");
+    answer3.className = "answer-three";
+    answer3.textContent = "3. var colors = 'red', 'green', 'blue'";
+    answerDiv.appendChild(answer3);
+    answer3.addEventListener("click", event => {
+        wrong();
+    });
+
+    var answer4 = document.createElement("button");
+    answer4.className = "answer-four";
+    answer4.textContent = "4. var colors = (1:'red', 2:'green', 3:'blue'";
+    answerDiv.appendChild(answer4);
+    answer4.addEventListener("click", event => {
+        wrong();
+    });
+};
+
+var questionTwo = function () {
+    var wrong = function () {
+        questionThree();
+        questionDiv.remove();
+        time = time - 10;
+    }
+
+    var questionDiv = document.createElement("div");
+    questionDiv.className = "questionDiv";
+    startEl.appendChild(questionDiv);
+
+    var question =document.createElement("h1");
+    question.className = "question";
+    question.textContent = "How to write an IF statement in JavaScript";
+    questionDiv.appendChild(question);
+
+    var answerDiv = document.createElement("div");
+    answerDiv.className = "answerDiv";
+    questionDiv.appendChild(answerDiv);
+
+    var answer1 = document.createElement("button");
+    answer1.className = "answer-one";
+    answer1.textContent = "1. if i=5";
+    answerDiv.appendChild(answer1);
+    answer1.addEventListener("click", event => {
+        wrong();
+    });
+
+    var answer2 = document.createElement("button");
+    answer2.className = "answer-two";
+    answer2.textContent = "2. if (i==5)";
+    answerDiv.appendChild(answer2);
+    answer2.addEventListener("click", event => {
+        questionDiv.remove();
+        questionThree();
+        points();
+    })
+
+    var answer3 = document.createElement("button");
+    answer3.className = "answer-three";
+    answer3.textContent = "3. if i==5 then";
+    answerDiv.appendChild(answer3);
+    answer3.addEventListener("click", event => {
+        wrong();
+    });
+
+    var answer4 = document.createElement("button");
+    answer4.className = "answer-four";
+    answer4.textContent = "4. if i=5 then";
+    answerDiv.appendChild(answer4);
+    answer4.addEventListener("click", event => {
+        wrong();
+    });
+};
+
+var questionOne = function() {
+
+    var wrong = function() {
+        time = time - 10;
+        questionDiv.remove();
+        questionTwo();
+    }
+
+    var questionDiv = document.createElement("div");
+    questionDiv.className = "questionDiv";
+    startEl.appendChild(questionDiv);
+
+    var question = document.createElement("h1");
+    question.textContent = "Inside which HTML element do we put the JavaScript?";
+    question.className = "question";
+    questionDiv.appendChild(question);
+
+    var answerDiv = document.createElement("div");
+    answerDiv.className = "answerDiv";
+    questionDiv.appendChild(answerDiv);
+
+    var answer1 = document.createElement("button");
+    answer1.textContent = "1. script";
+    answer1.className = "answer-one";
+    answerDiv.appendChild(answer1);
+    answer1.addEventListener("click", event => {
+        questionDiv.remove();
+        questionTwo();
+        points();
+    })
+
+
+
+    var answer2 = document.createElement("button");
+    answer2.textContent = "2. js";
+    answer2.className = "answer-two"
+    answerDiv.appendChild(answer2);
+    answer2.addEventListener("click", event => {
+        wrong();
+    });
+
+
+    var answer3 = document.createElement("button");
+    answer3.textContent = "3. scripting";
+    answer3.className = "answer-three"
+    answerDiv.appendChild(answer3);
+    answer3.addEventListener("click", event => {
+        wrong();
+    });
+
+
+
+    var answer4 = document.createElement("button");
+    answer4.textContent = "4. javascript";
+    answer4.className = "answer-four";
+    answerDiv.appendChild(answer4);
+    answer4.addEventListener("click", event => {
+        wrong();
+    });
+
+};
 
 var startQuiz = function () {
     var body = document.createElement("div");
