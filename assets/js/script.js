8b2fef49f8834fea8c1c4c32e4da8ab39210986a
IanAdamPrice <ianadamprice@gmail.com>
2022-04-04 17:03:31 -0400

    finalize

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,51 @@ var timer = function () {
   }, 1000);
 };
 
+var highScore = function () {
+  var highScoreDiv = document.createElement("div");
+  highScoreDiv.className = "highScore";
+  startEl.appendChild(highScoreDiv);
+      
+  var title = document.createElement("h1");
+  title.className = "high-score-title";
+  title.textContent = "High Scores";
+  highScoreDiv.appendChild(title);
+      
+  var initals = localStorage.getItem("initals");
+  var score = localStorage.getItem("result");
+  var leaderboardScore = document.createElement("p");
+  leaderboardScore.className = "leaderboard-score";
+  leaderboardScore.textContent = "1. " + initals + " - " + score;
+  highScoreDiv.appendChild(leaderboardScore);
+      
+      
+  var leaderboard = document.createElement("div");
+  leaderboard.className = "leaderboard";
+  highScoreDiv.appendChild(leaderboard);
+
+  var goBack = document.createElement("button");
+  goBack.className = "go-back-btn";
+  goBack.textContent = "Go Back";
+  goBack.addEventListener("click", event => {
+    totalPoints = 0;
+    end = false;
+    timeLeft = 75;
+    highScoreDiv.remove();
+    startQuiz();
+  })
+  leaderboard.appendChild(goBack);
+
+  var clearHighScore = document.createElement("button");
+  clearHighScore.className = "clear-highScore";
+  clearHighScore.textContent = "Clear High Scores";
+  clearHighScore.addEventListener("click", event => {
+    localStorage.removeItem("initals")
+    localStorage.removeItem("result")
+    leaderboardScore.remove();
+  })
+  leaderboard.appendChild(clearHighScore);
+}
+
 var yourScore = function () {
   var scoreDiv = document.createElement("div");
   scoreDiv.className = "scoreDiv";
@@ -45,7 +90,7 @@ var yourScore = function () {
   inputContainer.appendChild(label);
 
   var initalInput = document.createElement("Input");
-  initalInput.className = "score-input";
+  initalInput.className = "initial-input";
   initalInput.setAttribute("id", "input")
   initalInput.setAttribute("name", "input");
   initalInput.setAttribute("type", "text");
@@ -64,7 +109,7 @@ var yourScore = function () {
 
         scoreDiv.remove();
 
-        //highScores();
+        highScore();
     })
     inputContainer.appendChild(submit);
 
